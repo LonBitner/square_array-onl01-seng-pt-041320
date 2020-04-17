e1f01ac66ba6da733f8f9785b611dc9f15ba9e1a
Lon Bitner <lonbitner@gmail.com>
2020-04-17 21:17:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,11 @@ def square_array(array)
   array.each do |**|
 end
 end
+
+def square_array(array)
+  array = []
+ array.each do |integer|
+  new_array = [integer ** 2]
+  return new_array
+ end
+end
