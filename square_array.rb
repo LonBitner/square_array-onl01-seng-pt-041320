3f601d7315da4aed08aa7142d07518f08d7173c8
Lon Bitner <lonbitner@gmail.com>
2020-04-17 21:17:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,6 @@
-def square_array(array)
-  array = ["1", "2", "3"]
-  array.each do |**|
-end
-end
 
 def square_array(array)
-  array = []
+  array = ["1", "2", "3"]
  array.each do |integer|
   new_array = [integer ** 2]
   return new_array
